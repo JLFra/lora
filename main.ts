6659328c44a5da9ce25cda482340309b81c64f9d
julien launay <julien.launay@laposte.net>
2021-06-07 11:27:54 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,39 @@
+//% color="#037268" icon="\uf519"
+namespace lora{
+
+    //% block="Connexion au système Lora"
+    export function connect_lora(): void {
+        basic.showIcon(IconNames.Asleep)
+        basic.pause(500)
+        serial.setRxBufferSize(100)
+        serial.redirect(
+        SerialPin.P14,
+        SerialPin.P0,
+        BaudRate.BaudRate1200
+        )
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallSquare)
+        let reception = ""
+        while (reception == "") {
+            serial.writeString("?")
+            reception = serial.readUntil(serial.delimiters(Delimiters.Hash))
+            if (reception == "ok") { 
+                basic.showString("C")
+            }
+            else {
+                basic.showIcon(IconNames.Square)
+                basic.pause(500)	
+            }
+        }
+    }
+    //% block="Envoi $donnee"
+    //% donnee.defl='essai'
+    export function envoi_donnee(donnee: string): void {
+        serial.writeString(donnee+"#")
+    }
+
+    //% block="Donnee reçue"
+    export function donnee_recue(): string {
+        return serial.readUntil(serial.delimiters(Delimiters.Hash));
+    }
+}
